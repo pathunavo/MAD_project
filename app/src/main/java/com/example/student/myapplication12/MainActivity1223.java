6e42ed2cbe74ac01aa6cad21faf4c01d912951ce
pathunavo <pathu.navo96@gmail.com>
2019-07-20 12:26:16 +0530

    Commit all the files to the repository

---
+++ app/src/main/java/com/example/student/myapplication12/MainActivity1223.java
@@ -0,0 +1,13 @@
+package com.example.student.myapplication12;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity1223 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main1223);
+    }
+}
